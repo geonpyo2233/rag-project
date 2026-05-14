b1d577505e13a18351d00583c3d5d75ee3ca3e2c
oct <dhcksdid99@gmail.com>
2026-05-14 14:05:39 +0900

    ocr 구현 완료

--- backend/experiments/oh/src/main.py
+++ backend/experiments/oh/src/main.py
@@ -3,16 +3,17 @@ from pathlib import Path
 from check_file import check_input_file
 from convert_pdf import convert_docx_to_pdf
 from pdf_to_images import convert_pdf_to_images
+from run_ocr import run_ocr_on_images
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATA_DIR = BASE_DIR / "data"
-
 OUTPUT_DIR = BASE_DIR / "output"
 
 PDF_DIR = OUTPUT_DIR / "pdf"
 IMAGE_DIR = OUTPUT_DIR / "images"
+TEXT_DIR = OUTPUT_DIR / "text"
 
 
 def main():
@@ -30,10 +31,13 @@ def main():
         IMAGE_DIR
     )
 
-    print("\n===== STEP 03 COMPLETE =====")
+    result_path = run_ocr_on_images(
+        image_paths,
+        TEXT_DIR
+    )
 
-    for image_path in image_paths:
-        print(image_path.name)
+    print("\n===== STEP 04 COMPLETE =====")
+    print(f"OCR 결과 파일: {result_path}")
 
 
 if __name__ == "__main__":
