30ec7a332a61902804130d2f0ba702d511dc392e
oct <dhcksdid99@gmail.com>
2026-05-14 15:15:25 +0900

    docx -> 텍스트는 직접 추출 이미지는 ocr로 처리

--- backend/experiments/oh/src/main.py
+++ backend/experiments/oh/src/main.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 
 from check_file import check_input_file
-from convert_pdf import convert_docx_to_pdf
-from pdf_to_images import convert_pdf_to_images
-from run_ocr import run_ocr_on_images
+from pipelines.docx_pipeline import run_docx_hybrid_pipeline
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,33 +9,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / "data"
 OUTPUT_DIR = BASE_DIR / "output"
 
-PDF_DIR = OUTPUT_DIR / "pdf"
-IMAGE_DIR = OUTPUT_DIR / "images"
-TEXT_DIR = OUTPUT_DIR / "text"
-
 
 def main():
     print("===== OCR PIPELINE START =====")
 
     selected_file = check_input_file(DATA_DIR)
 
-    pdf_path = convert_docx_to_pdf(
+    result_path = run_docx_hybrid_pipeline(
         selected_file,
-        PDF_DIR
+        OUTPUT_DIR
     )
 
-    image_paths = convert_pdf_to_images(
-        pdf_path,
-        IMAGE_DIR
-    )
-
-    result_path = run_ocr_on_images(
-        image_paths,
-        TEXT_DIR
-    )
-
-    print("\n===== STEP 04 COMPLETE =====")
-    print(f"OCR 결과 파일: {result_path}")
+    print("\n===== COMPLETE =====")
+    print(f"최종 결과 파일: {result_path}")
 
 
 if __name__ == "__main__":
